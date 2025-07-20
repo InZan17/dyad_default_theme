cb6ff9fc44ca6813789706c7870fbc460b8b3234
Zan <69921455+InZan17@users.noreply.github.com>
2025-07-20 03:36:24 +0200

    show player skin (ish)

--- scripts/playerlist.lua
+++ scripts/playerlist.lua
@@ -4,9 +4,18 @@ return Create.list{
     color = Color.from_rgb(0.6,0.6,0.6),
     on_init=function(self)
         for i, account in ipairs(account_manager:get_available_accounts()) do
-            Create.text{
-                text_alignment =  Enum.TextAlignment.MiddleLeft,
-                text = "Username: "..account.username.."\nuuid: "..account.uuid,
+            Create.frame{
+                Create.quad{
+                    scale=UDim2.new(pct(100), pct(100)),
+                    on_init=function(self)
+                        self.texture = account.skin
+                    end
+                },
+                Create.text{
+                    scale=UDim2.new(pct(100), pct(100)),
+                    text_alignment =  Enum.TextAlignment.MiddleLeft,
+                    text = "Username: "..account.username.."\nuuid: "..account.uuid,
+                }
             }.parent = self
         end
     end
