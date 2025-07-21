534eea7b80bbf61476c7b7259ef47c0660437746
Zan <69921455+InZan17@users.noreply.github.com>
2025-07-21 16:45:37 +0200

    show player head

--- scripts/playerlist.lua
+++ scripts/playerlist.lua
@@ -1,18 +1,19 @@
 return Create.list{
     scale = UDim2.new(pct(100), pct(100)),
-    child_size = UDim2.new(pct(100), inch(1)),
+    child_size = UDim2.new(pct(100), inch(0.75)),
     color = Color.from_rgb(0.6,0.6,0.6),
     on_init=function(self)
         for i, account in ipairs(account_manager:get_available_accounts()) do
             Create.frame{
-                Create.quad{
-                    scale=UDim2.new(pct(100), pct(100)),
-                    on_init=function(self)
-                        self.texture = account.skin
-                    end
-                },
+                create_player_head(account.skin, {
+                    scale=UDim2.new(inch(0.75), inch(0.75)),
+                    position=UDim2.new(inch(0.1), 0),
+                }),
                 Create.text{
-                    scale=UDim2.new(pct(100), pct(100)),
+                    scale=UDim2.new(pct(100) - inch(1), pct(100)),
+                    position=UDim2.new(pct(100), 0),
+                    anchor=Vec2.new(1, 0),
+                    font_size = inch(0.2),
                     text_alignment =  Enum.TextAlignment.MiddleLeft,
                     text = "Username: "..account.username.."\nuuid: "..account.uuid,
                 }
---
+++ startup/create_player_head.lua
@@ -0,0 +1,28 @@
+create_player_head = function(texture, properties)
+    local frame_table = {
+        Create.quad{
+            scale=UDim2.new(pct(100), pct(100)),
+            uv_scale=Vec2.new(1 / 8, 1 / 8),
+            uv_offset=Vec2.new(5 / 8, 6 / 8),
+            anchor=Vec2.new(0.5, 0.5),
+            position=UDim2.new(pct(50), pct(50)),
+            on_init=function(self)
+                self.texture = texture
+            end
+        },
+        Create.quad{
+            scale=UDim2.new(pct(91), pct(91)),
+            uv_scale=Vec2.new(1 / 8, 1 / 8),
+            uv_offset=Vec2.new(1 / 8, 6 / 8),
+            anchor=Vec2.new(0.5, 0.5),
+            position=UDim2.new(pct(50), pct(50)),
+            on_init=function(self)
+                self.texture = texture
+            end
+        },
+    }
+
+    for k,v in pairs(frame_table) do properties[k] = v end
+
+    return Create.frame(properties)
+end
