2af242f8fdf16109d842e01f0b2d2b4f5a27e423
Zan <69921455+InZan17@users.noreply.github.com>
2025-07-23 04:39:23 +0200

    fix the player head texture leaking

--- startup/create_player_head.lua
+++ startup/create_player_head.lua
@@ -1,9 +1,11 @@
+local small_number = 0.001
+
 create_player_head = function(texture, properties)
     local frame_table = {
         Create.quad{
             scale=UDim2.new(pct(100), pct(100)),
-            uv_scale=Vec2.new(1 / 8, 1 / 8),
-            uv_offset=Vec2.new(5 / 8, 6 / 8),
+            uv_scale=Vec2.new(1 / 8 - small_number, 1 / 8 - small_number),
+            uv_offset=Vec2.new(5 / 8 + small_number / 2, 6 / 8 + small_number / 2),
             anchor=Vec2.new(0.5, 0.5),
             position=UDim2.new(pct(50), pct(50)),
             on_init=function(self)
@@ -12,8 +14,8 @@ create_player_head = function(texture, properties)
         },
         Create.quad{
             scale=UDim2.new(pct(90), pct(90)),
-            uv_scale=Vec2.new(1 / 8, 1 / 8),
-            uv_offset=Vec2.new(1 / 8, 6 / 8),
+            uv_scale=Vec2.new(1 / 8 - small_number, 1 / 8 - small_number),
+            uv_offset=Vec2.new(1 / 8 + small_number / 2, 6 / 8 + small_number / 2),
             anchor=Vec2.new(0.5, 0.5),
             position=UDim2.new(pct(50), pct(50)),
             on_init=function(self)
@@ -22,8 +24,8 @@ create_player_head = function(texture, properties)
         },
         Create.quad{
             scale=UDim2.new(pct(95), pct(95)),
-            uv_scale=Vec2.new(-1 / 8, 1 / 8),
-            uv_offset=Vec2.new(8 / 8, 6 / 8),
+            uv_scale=Vec2.new(-1 / 8 - small_number, 1 / 8 - small_number),
+            uv_offset=Vec2.new(8 / 8 + small_number / 2, 6 / 8 + small_number / 2),
             anchor=Vec2.new(0.5, 0.5),
             position=UDim2.new(pct(50), pct(50)),
             on_init=function(self)
